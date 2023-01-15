d75670b7a687873e6b081a058da0dbcf14fa6cf3
Vignesh <vignesh.mariappan@anywhere.co>
2023-01-15 13:33:15 +0530

    Refactor: Added Example Project

---
+++ Example/Saved.ly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saved.ly
+//
+//  Created by Vignesh on 15/01/23.
+//
+
+import UIKit
+import Papyrus
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
